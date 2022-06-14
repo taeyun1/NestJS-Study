142b77cfede7449841b18b5d6f29f9e59037d4e7
yootaeyun <ew20@naver.com>
2022-06-15 01:28:21 +0900

    NestJS : CRUD구현, 게시물 생성하기(Controller부분)

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,18 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
-	constructor(private boardsService: BoardsService) {
-
-	}
+	constructor(private boardsService: BoardsService) {}
 
 	// 모든 게시물을 가져오는 핸들러 생성
-	@Get()
+	@Get('/')
 	getAllBoards(): Board[] {
 		return this.boardsService.getAllBoards();
 	}
+
+	@Post() 
+		createBoard(
+			@Body('title') title: string,
+			@Body('description') description: string,
+			): Board {
+				// boardsService의 createBoard를 가져와 title, description 전달
+				return this.boardsService.createBoard(title, description);
+
+		}
+	
 
 
 }
