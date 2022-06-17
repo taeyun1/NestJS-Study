247c202eb5cd39bff3dc4737f7d4a90ccd7c736b
yootaeyun <ew20@naver.com>
2022-06-17 18:35:59 +0900

    NestJS : Repository 생성하기

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardRepository } from './boadrd.reopsitory';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 
 @Module({
+	imports: [
+		TypeOrmModule.forFeature([BoardRepository])
+	],
   controllers: [BoardsController],
   providers: [BoardsService]
 })
